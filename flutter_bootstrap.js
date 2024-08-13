d342930907c8c27472de68e80afb9115ad36eae9
Henil <henil.p@smartnode.in>
2024-08-13 16:15:06 +0530

    [IMPL] arrow change

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "765991013"
+    serviceWorkerVersion: "3372867362"
   }
 });
